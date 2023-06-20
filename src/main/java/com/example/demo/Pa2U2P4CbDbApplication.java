b06d1314e21e9287b90e6babe8d7ad6b1724fed3
Chriztian2000 <cabetancourt@uce.edu.ec>
2023-06-20 10:21:05 -0500

    Tarea6_BetancourtChristian

--- src/main/java/com/example/demo/Pa2U2P4CbDbApplication.java
+++ src/main/java/com/example/demo/Pa2U2P4CbDbApplication.java
@@ -40,53 +40,28 @@ public class Pa2U2P4CbDbApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		/*
-		 * Hotel hote1 = new Hotel(); hote1.setNombre("Necochea");
-		 * hote1.setDireccion("Los Dos Puentes");
-		 * 
-		 * List<Habitacion> habitaciones = new ArrayList<>();
-		 * 
-		 * 
-		 * 
-		 * Habitacion habi1 = new Habitacion(); habi1.setNumero("1A");
-		 * habi1.setValor(new BigDecimal(100)); habi1.setHotel(hote1);
-		 * habitaciones.add(habi1);
-		 * 
-		 * Habitacion habi2 = new Habitacion(); habi2.setNumero("2A");
-		 * habi2.setValor(new BigDecimal(90)); habi2.setHotel(hote1);
-		 * habitaciones.add(habi2);
-		 * 
-		 * //agregar a la lista de habitaciones en en hotel
-		 * hote1.setHabitaciones(habitaciones);
-		 * 
-		 * this.hotelService.actualizar(hote1);
-		 */
 
 		Set<Autor> autorSetList = new HashSet<>();
-
 		Set<Libro> libroSetList = new HashSet<>();
 
 		Autor autor = new Autor();
-
 		autor.setNombre("Charles");
-
 		autor.setApellid("Bukowsky");
-
 		autorSetList.add(autor);
-
 		autor.setLibros(libroSetList);
 
 		Libro libro = new Libro();
-
 		libro.setTitulo("historias agonicas");
-
 		libro.setEditorial("Bermeu");
-
 		libroSetList.add(libro);
-
 		libro.setAutores(autorSetList);
 
 		this.autorService.insertar(autor);
+		
+		autor.setNombre("Christopher");
+		autor.setApellid("Nolan");
+		this.autorService.actualizar(autor);
+
 
 	}
 }

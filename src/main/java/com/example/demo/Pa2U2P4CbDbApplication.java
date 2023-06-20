43bd9dc5cac762047c63297f6c55bea2b6e80481
Chriztian2000 <cabetancourt@uce.edu.ec>
2023-06-20 10:58:49 -0500

    tarea4_BetancourtChrisitan

--- src/main/java/com/example/demo/Pa2U2P4CbDbApplication.java
+++ src/main/java/com/example/demo/Pa2U2P4CbDbApplication.java
@@ -1,17 +1,16 @@
 package com.example.demo;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.demo.biblio.repo.modelo.Autor;
-import com.example.demo.biblio.repo.modelo.Libro;
 import com.example.demo.biblio.service.IAutorService;
 import com.example.demo.biblio.service.ILibroService;
+import com.example.demo.repository.modelo.Ciudadano;
+import com.example.demo.repository.modelo.Empleado;
 import com.example.demo.service.CiudadanoService;
 import com.example.demo.service.HabitacionService;
 import com.example.demo.service.HotelService;
@@ -40,53 +39,31 @@ public class Pa2U2P4CbDbApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		/*
-		 * Hotel hote1 = new Hotel(); hote1.setNombre("Necochea");
-		 * hote1.setDireccion("Los Dos Puentes");
-		 * 
-		 * List<Habitacion> habitaciones = new ArrayList<>();
-		 * 
-		 * 
-		 * 
-		 * Habitacion habi1 = new Habitacion(); habi1.setNumero("1A");
-		 * habi1.setValor(new BigDecimal(100)); habi1.setHotel(hote1);
-		 * habitaciones.add(habi1);
-		 * 
-		 * Habitacion habi2 = new Habitacion(); habi2.setNumero("2A");
-		 * habi2.setValor(new BigDecimal(90)); habi2.setHotel(hote1);
-		 * habitaciones.add(habi2);
-		 * 
-		 * //agregar a la lista de habitaciones en en hotel
-		 * hote1.setHabitaciones(habitaciones);
-		 * 
-		 * this.hotelService.actualizar(hote1);
-		 */
+		
+		Ciudadano ciudadano = new Ciudadano();
+		ciudadano.setNombre("Christian");
+		ciudadano.setApellido("Betancourt");
+		ciudadano.setCedula("1722781000");
 
-		Set<Autor> autorSetList = new HashSet<>();
+		Ciudadano ciudadano1 = new Ciudadano();
+		ciudadano1.setNombre("David");
+		ciudadano1.setApellido("Boada");
+		ciudadano1.setCedula("123456789");
 
-		Set<Libro> libroSetList = new HashSet<>();
+		Empleado empleado = new Empleado();
+		empleado.setCiudadano(ciudadano1);
+		empleado.setCargo("Gerente");
+		empleado.setSueldo(new BigDecimal(2000));
+		
+        this.ciudadanoService.insertar(ciudadano);
+        
+        ciudadano.setNombre("Alejandro");
+        this.ciudadanoService.actualizar(ciudadano);
+        
+      
+        
 
-		Autor autor = new Autor();
-
-		autor.setNombre("Charles");
-
-		autor.setApellid("Bukowsky");
-
-		autorSetList.add(autor);
-
-		autor.setLibros(libroSetList);
-
-		Libro libro = new Libro();
-
-		libro.setTitulo("historias agonicas");
-
-		libro.setEditorial("Bermeu");
-
-		libroSetList.add(libro);
-
-		libro.setAutores(autorSetList);
-
-		this.autorService.insertar(autor);
+		
 
 	}
 }

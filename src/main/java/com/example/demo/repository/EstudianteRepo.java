9fd81ebc9dcbff042de31a8ec586fecd3579d548
Chriztian2000 <cabetancourt@uce.edu.ec>
2023-07-07 16:40:03 -0500

    deberUpdate

--- src/main/java/com/example/demo/repository/EstudianteRepo.java
+++ src/main/java/com/example/demo/repository/EstudianteRepo.java
@@ -6,45 +6,47 @@ import com.example.demo.repository.modelo.Estudiante;
 
 public interface EstudianteRepo {
 	public void insertar(Estudiante estudiante);
-	public Estudiante buscar (String cedula);
+
+	public Estudiante buscar(String cedula);
+
 	public void eliminar(String cedula);
+
 	public void actualizar(Estudiante estudiante);
-	
-	public Estudiante seleccionarporApellido (String apellido,String nombre);
-	public Estudiante seleccionarporApellidoType (String apellido);
+
+	public Estudiante seleccionarporApellido(String apellido, String nombre);
+
+	public Estudiante seleccionarporApellidoType(String apellido);
 
 	public List<Estudiante> seleccionarListaPorApellido(String apellido);
-	
-	public Estudiante seleccionarporApellidoNamed (String apellido);
-	
-	public Estudiante seleccionarporApellidoNamedQuery (String apellido);
+
+	public Estudiante seleccionarporApellidoNamed(String apellido);
+
+	public Estudiante seleccionarporApellidoNamedQuery(String apellido);
 
 	public Estudiante seleccionarPorApellidoNativeQuery(String apellido);
-	
+
 	public Estudiante seleccionarPorApellidoNativeQueryNamed(String apellido);
-	
-	
-	public Estudiante seleccionarporNombreNamedQuery (String nombre);
+
+	public Estudiante seleccionarporNombreNamedQuery(String nombre);
+
 	public Estudiante seleccionarPorNombreNativeQueryNamed(String nombre);
-	
-	
-	
-	public Estudiante seleccionarporApellidoCriteriaApiQuery (String apellido);
-	
+
+	public Estudiante seleccionarporApellidoCriteriaApiQuery(String apellido);
+
 	public Estudiante seleccionarEstudianteDinamico(String nombre, String apellido, Double peso);
-	
+
 	public int eliminarPorNombre(String nombre);
-	
-	//desde un apellido se cambia el nombre
-	public int actualizarPorApellido(String nombre,String apellido);
-	
+
+	// desde un apellido se cambia el nombre
+	public int actualizarPorApellido(String nombre, String apellido);
+
+	// deber
+	public int eliminarPorEdad(Integer edad);
+
+	// desde una edad se cambia el establecimiento
+	public int actualizarPorEdad(String establecimiento, Integer edad);
 	
 	//deber
-	public int eliminarPorEdad(Integer edad);
 	
-	//desde una edad se cambia el establecimiento
-		public int actualizarPorEdad(String establecimiento,Integer edad);
-	
-	
-	
+
 }
